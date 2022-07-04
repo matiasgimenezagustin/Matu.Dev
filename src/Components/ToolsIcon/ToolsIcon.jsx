3a67a0e7f96b162e4b3e1dd4cc6282d334d3ea0a
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-07-04 12:17:31 -0300

    works component built

---
+++ src/Components/ToolsIcon/ToolsIcon.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import html5Icon from "../../img/icons-tools/html5-icon.svg"
+import css3Icon from "../../img/icons-tools/css3-icon.svg"
+import jsIcon from "../../img/icons-tools/js-icon.svg"
+import reactIcon from "../../img/icons-tools/react-icon.svg"
+import bootstrapIcon from "../../img/icons-tools/bootstrap-icon.svg"
+import "./toolsIcon.css"
+function Tool(props) {
+
+    let iconSelect;
+    let txtSelect;
+
+
+    switch(props.tool){
+
+        case "htmlIcon":
+            iconSelect = html5Icon;
+            txtSelect = "Html5";
+            break;
+
+        case "cssIcon":
+            iconSelect = css3Icon;
+            txtSelect = "Css3";
+            break;
+
+        case "reactIcon":
+            iconSelect = reactIcon;
+            txtSelect = "React JS";
+            break;
+
+        case "jsIcon":
+            iconSelect = jsIcon;
+            txtSelect = "Js";
+            break;
+
+        case "bootstrapIcon":
+            iconSelect = bootstrapIcon;
+            txtSelect = "Bootstrap";
+            break;
+
+    }
+    
+    return (
+        <span className={`workIconContainer ${props.tool}` }>
+            <span>{txtSelect}</span>
+            <img src={iconSelect} alt="" className='workIcon' />
+        </span>
+    );
+}
+
+export default Tool;
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import "./works.css"
 import { useContextProvider} from '../../Context/ContextProvider';
+import Tool from '../ToolsIcon/ToolsIcon';
 
 import html5Icon from "../../img/icons-tools/html5-icon.svg"
 import css3Icon from "../../img/icons-tools/css3-icon.svg"
@@ -15,54 +16,73 @@ function Works() {
     const handleChangeWorkSubtract = () =>{
         work === 0 ? setWork(0) : (setWork(work - 1))
     }
+
+
+
+
+
+    const WORK0 =
+        <section className='workSection'>
+            <div className="workTitleContainer">
+                <h3 className='workTitle'>{lenguages ? "React E-Commerce: Chill Store" : "E-Commerce en React: Chill Store"}</h3>
+            </div>
+            <div className='ytIframeContainer'>
+                <button className='btnWork left' onClick={handleChangeWorkSubtract}>{"<"}</button>
+                <iframe src="https://www.youtube.com/embed/annerNjihdY" width="560" height="415"  title="YouTube video player" frameBorder="0" className='ytIframe' allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <button className='btnWork rigth' onClick={handleChangeWorkAdd}>{">"}</button>
+            </div>
+            <div className='workDescription'>
+                <div className='toolsContainer'>
+                    <h4 className='toolsSubtitle'>{lenguages ? "Tools: " : "Herramientas: "}</h4>
+                    <Tool tool="htmlIcon"/>
+                    <Tool tool="cssIcon"/>
+                    <Tool tool="reactIcon"/>
+                    <Tool tool="jsIcon"/>
+                </div>
+            </div>
+        </section>;
+
     const WORK1 =
         <section className='workSection'>
-            <h3 className='workTitle'>{lenguages ? "React E-Commerce Work" : "E-Commerce en React"}</h3>
-            <p>{work === 0 ? "work en 0" : "work en 1"}</p>
-            <button >click me</button>
+            <div className="workTitleContainer">
+                <h3 className='workTitle'>{lenguages ? "Freelance Proyect" : "Proyecto Freelance"}</h3>
+            </div>
+            <div className='ytIframeContainer'>
+                <button className='btnWork left' onClick={handleChangeWorkSubtract}>{"<"}</button>
+                <iframe width="560" height="415" className='ytIframe' src="https://www.youtube.com/embed/pf8zcBXixSw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <button className='btnWork rigth' onClick={handleChangeWorkAdd}>{">"}</button>
+            </div>
+            <div className='toolsContainer'>
+                <h4 className='toolsSubtitle'>{lenguages ? "Tools: " : "Herramientas: "}</h4>
+                <Tool tool="htmlIcon"/>
+                <Tool tool="cssIcon"/>
+                <Tool tool="bootstrapIcon"/>
+            </div>
         </section>;
-    
+    const WORK2 = 
+        <section className='workSection'>
+            <div className="workTitleContainer">
+                <h3 className='workTitle'>{lenguages ? "Web Desing proyect: Flow Royale" : "Proyecto de diseño web: Flow Royale"}</h3>
+            </div>
+            <div className='ytIframeContainer'>
+                <button className='btnWork left' onClick={handleChangeWorkSubtract}>{"<"}</button>
+                <iframe width="560" height="415" className='ytIframe' src="https://www.youtube.com/embed/uZpUw16BWfQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <button className='btnWork rigth' onClick={handleChangeWorkAdd}>{">"}</button>
+            </div>
+            <div className='toolsContainer'>
+                <h4 className='toolsSubtitle'>{lenguages ? "Tools: " : "Herramientas: "}</h4>
+                <Tool tool="htmlIcon"/>
+                <Tool tool="cssIcon"/>
+                <Tool tool="bootstrapIcon"/>
+            </div>
+        </section>;
 
     return (
         <div className='worksContainer'>
             <h2 className='subTitle subTitleW'>{lenguages ? "Works" : "Trabajos"}</h2>
-            <section className='workSection'>
-                <div className="workTitleContainer">
-                    <h3 className='workTitle'>{lenguages ? "React E-Commerce: Chill Store" : "E-Commerce en React: Chill Store"}</h3>
-                </div>
-                <div className='ytIframeContainer'>
-
-                    <button className='btnWork left' onClick={handleChangeWorkSubtract}>{"<"}</button>
-
-                    <iframe src="https://www.youtube.com/embed/annerNjihdY" width="560" height="415"  title="YouTube video player" frameBorder="0" className='ytIframe' allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-
-                    <button className='btnWork rigth' onClick={handleChangeWorkAdd}>{">"}</button>
-
-                </div>
-                
-                <div className='workDescription'>
-                    <div className='toolsContainer'>
-                        <h4 className='toolsSubtitle'>{lenguages ? "Tools: " : "Herramientas: "}</h4>
-                        <span className='workIconContainer htmlIcon'>
-                            <span>html5</span>
-                            <img src={html5Icon} alt="" className='workIcon '/>
-                        </span>
-                        <span className='workIconContainer cssIcon'>
-                            <span>css3</span>
-                            <img src={css3Icon} alt="" className='workIcon '/>
-                        </span>
-                        <span className='workIconContainer reactIcon'>
-                            <span>react JS</span>
-                            <img src={reactIcon} alt="" className='workIcon '/>
-                        </span>
-                        <span className='workIconContainer jsIcon'>
-                            <span>Js</span>
-                            <img src={jsIcon} alt="" className='workIcon '/>
-                        </span>
-                    </div>
-                </div>
-                
-            </section>
+            {
+                work === 0 ? WORK0 : (work === 1 ?  WORK1 : WORK2)
+            }
         </div>
         
     );
