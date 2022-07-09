9c28f6ed938efa579da08e11af25953c2f866389
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-07-08 23:08:14 -0300

    yeah

--- src/Components/Foooter/Footer.jsx
+++ src/Components/Foooter/Footer.jsx
@@ -8,11 +8,10 @@ import freeCampIcon from "../../img/social/freeCampIcon.svg";
 const Footer = (props) => {
     return (
         <footer className='footer'>
-            <img src={wspIcon} alt="" />
-            <img src={gmailIcon} alt="" />
+            <a href="https://wa.me/5491122491671?text=¡hi!"><img src={wspIcon} alt="whatsapp icon" /></a>
+            <a href="https://www.linkedin.com/in/matias-agustin-gimenez-2515091b7/"><img src={lkIcon} alt="linkedin icon" /></a>
+            <a href="https://www.freecodecamp.org/Matudev"><img src={freeCampIcon} alt="free code camp icon" /></a>
             <h4>Matu.Dev</h4>
-            <img src={lkIcon} alt="" />
-            <img src={freeCampIcon} alt="" />
         </footer>
     );
 }
